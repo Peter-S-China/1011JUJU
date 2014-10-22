65ebfaad83990d007d360234596ab9b05ff03d16
Peter <65519035@qq.com>
2014-10-22 18:01:54 +0800

    1022

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1917,6 +1917,29 @@ function jgchstep1GameCtrl1($scope,$timeout,$rootScope,$location){
          $location.path("/jgchview2");
     
     }
+    
+    
+    
+    
+    
+    function playAudio(url) {
+        // Play the audio file at url
+        var my_media = new Media(url,
+                                 // success callback
+                                 function() {
+                                 console.log("playAudio():Audio Success");
+                                 },
+                                 // error callback
+                                 function(err) {
+                                 console.log("playAudio():Audio Error: "+err);
+                                 });
+        
+        // Play audio
+        my_media.play();
+    }
+    
+    playAudio("partials/jgch/readygo.mp3");
+
 }
 
 
